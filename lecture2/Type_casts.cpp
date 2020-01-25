ee22b866d8f2a152230e208b1d155b9583243367
Your Name <you@example.com>
2020-01-25 17:49:19 +0000

    lecture1 and lecture2 programming

---
+++ lecture2/Type_casts.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream> 
+using namespace std; 
+
+int main() {
+    int a, b, c;
+    a = 5;
+    b = 2;
+    cout << "Enter TEN integers: ";
+    cout << endl;
+    cin >> a >> b;
+    c = a + b;
+    cout << a << "+" << b << "=" << c;
+    cout << endl;
+
+}
+	
---
+++ lecture2/lecture2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+    int main() {
+        string command = "continue";
+       int counter = 0;
+       while(command = = "continue"){
+           cout << counter;
+           cout << "Enter continue to repeat or exit to stop:";
+           cout << endl;
+           cin >> command;
+       }
+       
+}
+
+
